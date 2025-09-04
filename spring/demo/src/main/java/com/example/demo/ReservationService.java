d3164b4e24e2923892d933c7e848ea227e5a3116
pyrozhenkko <149211193+pyrozhenkko@users.noreply.github.com>
2025-09-04 23:36:36 +0300

    spring boot jpa pt.1.3

--- spring/demo/src/main/java/com/example/demo/ReservationService.java
+++ spring/demo/src/main/java/com/example/demo/ReservationService.java
@@ -11,16 +11,13 @@ import java.util.logging.Logger;
 
 @Service
 public class ReservationService {
-    private final Map<Long, Reservation> reservationMap;
-    private final AtomicLong idCounter;
+
     private final ReservationRepository repository;
 
     private final static Logger logger = Logger.getLogger(ReservationService.class.getName());
 
     public ReservationService(ReservationRepository repository) {
         this.repository = repository;
-        reservationMap = new HashMap<>();
-        idCounter = new AtomicLong();
     }
 
     public Reservation getReservationById(Long id) {
@@ -65,77 +62,65 @@ public class ReservationService {
             Long id,
             Reservation reservationToUpdate
     ) {
-        if(!reservationMap.containsKey(id)) {
-            throw new NoSuchElementException("Not found reservation with id = " + id);
+
+        var reservationEntity = repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Not found reservation with id = " + id));
+
+        if (reservationEntity.getStatus() != ReservationStatus.PENDING) {
+            throw new IllegalStateException("reservation status is not PENDING, current status is " + reservationEntity.getStatus());
         }
-        if(repository.existsById(id)) {
-            throw new EntityNotFoundException("Not found reservation with id = " + id);
-        }
-        var reservationEntity = repository.findById(id);
-        var reservation = reservationMap.get(id);
-        if (reservation.status() != ReservationStatus.PENDING) {
-            throw new IllegalStateException("reservation status is not PENDING, current status is " + reservation.status());
-        }
-        var updatedReservation = new Reservation(
-                reservation.id(),
+        var reservationToSave = new ReservationEntity(
+                reservationEntity.getId(),
                 reservationToUpdate.userId(),
                 reservationToUpdate.roomId(),
                 reservationToUpdate.startDate(),
                 reservationToUpdate.endDate(),
                 ReservationStatus.PENDING
         );
-        reservationMap.put(reservation.id(), updatedReservation);
-        return updatedReservation;
+        var updatedReservation = repository.save(reservationToSave);
+        return toDomainReservation(updatedReservation);
     }
 
     public void deleteReservation(Long id) {
-        if(!reservationMap.containsKey(id)) {
-            logger.info("Not found reservation with id = " + id);
-            throw new NoSuchElementException("Not found reservation with id " + id);
+
+        if(!repository.existsById(id)) {
+            throw new EntityNotFoundException("Not found reservation with id = " + id);
         }
-        reservationMap.remove(id);
+        repository.deleteById(id);
 
     }
 
     public Reservation approveReservation(Long id) {
-        if(!reservationMap.containsKey(id)) {
-            throw new NoSuchElementException("Not found reservation with id " + id);
-        }
-        var reservation = reservationMap.get(id);
-        if (reservation.status() != ReservationStatus.PENDING) {
+        var reservationEntity = repository.findById(id)
+                .orElseThrow( () -> new EntityNotFoundException("Not found reservation by id = " + id));
+        if (reservationEntity.getStatus() != ReservationStatus.PENDING) {
             throw new IllegalStateException("reservation status is not PENDING, cannot approve reservation");
         }
-        var isConflict =  isReservationConflict(reservation);
+        var isConflict =  isReservationConflict(reservationEntity);
         if(isConflict) {
             throw new IllegalStateException("Cannot approve reservation because of conflict");
         }
-        var  approvedReservation = new Reservation(
-                reservation.id(),
-                reservation.userId(),
-                reservation.roomId(),
-                reservation.startDate(),
-                reservation.endDate(),
-                ReservationStatus.APPROVED
-        );
-        reservationMap.put(reservation.id(), approvedReservation);
-        return approvedReservation;
+        reservationEntity.setStatus(ReservationStatus.APPROVED);
+        repository.save(reservationEntity);
+        return toDomainReservation(reservationEntity);
     }
 
-    private boolean isReservationConflict(Reservation reservation) {
-        for (Reservation existing : reservationMap.values()) {
-            if (existing.id().equals(reservation.id())) {
+    private boolean isReservationConflict(ReservationEntity reservation) {
+        var allReservations = repository.findAll();
+        for (ReservationEntity existingReservation : allReservations) {
+            if (reservation.getId().equals(existingReservation.getId())) {
                 continue; // пропускаємо саму себе
             }
-            if (!Objects.equals(existing.roomId(), reservation.roomId())) {
+            if (!reservation.getRoomID().equals(existingReservation.getRoomID())) {
                 continue; // інша кімната — нас не цікавить
             }
-            if (existing.status() != ReservationStatus.APPROVED) {
+            if (!existingReservation.getStatus().equals(ReservationStatus.APPROVED)) {
                 continue; // конфліктуємо лише з уже підтвердженими
             }
 
             // Перетин дат: (start < other.end) && (other.start < end)
-            if (reservation.startDate().isBefore(existing.endDate()) &&
-                    existing.startDate().isBefore(reservation.endDate())) {
+            if (reservation.getStartDate().isBefore(existingReservation.getEndDate()) &&
+                    existingReservation.getStartDate().isBefore(reservation.getEndDate())) {
                 return true;
             }
         }
