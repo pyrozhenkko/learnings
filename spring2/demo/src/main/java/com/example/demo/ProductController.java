2efdf0fa8dab328d2c1b42b69af7efffe65550a2
pyrozhenkko <149211193+pyrozhenkko@users.noreply.github.com>
2025-09-23 15:42:19 +0300

    BuySell | minor changes in ui (problems with ui/lombok)

--- spring2/demo/src/main/java/com/example/demo/ProductController.java
+++ spring2/demo/src/main/java/com/example/demo/ProductController.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.models.Product;
 import com.example.demo.services.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProductController {
@@ -15,5 +17,10 @@ public class ProductController {
     public String products(Model model) {
         model.addAttribute("products", productService.getProducts());
         return "products";
+    }
+
+    @PostMapping("/product/create")
+    public String createProduct(Product product){
+
     }
 }
