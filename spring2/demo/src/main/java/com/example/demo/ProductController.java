ada545db433bcdeb67d1c6fdae474ca97bb0ff48
pyrozhenkko <149211193+pyrozhenkko@users.noreply.github.com>
2025-09-25 23:58:19 +0300

    BuySell | changes in controller(same problems)

--- spring2/demo/src/main/java/com/example/demo/ProductController.java
+++ spring2/demo/src/main/java/com/example/demo/ProductController.java
@@ -5,6 +5,7 @@ import com.example.demo.services.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -21,5 +22,17 @@ public class ProductController {
 
     @PostMapping("/product/create")
     public String createProduct(Product product){
+        productService.addProduct(product);
+        return "redirect:/";
+    }
+    @PostMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable Long id){
+        productService.deleteProduct(id);
+        return "redirect:/";
+    }
+    @GetMapping
+    public String productInfo(@PathVariable Long id, Model model) {
+        model.addAttribute("product", productService.getProductById(id));
+        return "productInfo";
     }
 }
