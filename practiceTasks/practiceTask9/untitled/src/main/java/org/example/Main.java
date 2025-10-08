f98b530a2574a352e58e84d86aed322a2ac1d930
pyrozhenkko <pyrozhenkko@gmail.com>
2025-10-08 23:57:40 +0300

    242. Valid Anagram

---
+++ practiceTasks/practiceTask9/untitled/src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        IO.println(String.format("Hello and welcome!"));
+
+        for (int i = 1; i <= 5; i++) {
+             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            IO.println("i = " + i);
+        }
+    }
+}
+public boolean isAnagram(String s, String t) {
+        if (s.length() !=t.length()) {
+            return false;
+        }
+        int count[] = new int[26];
+        for (int i=0;i<s.length();i++) {
+            char ch = s.charAt(i);
+            count[ch - 'a']++;
+        }
+        for (int i=0;i<t.length();i++) {
+            char ch = t.charAt(i);
+            count[ch - 'a']--;
+        }
+        for (int i=0; i <count.length; i++) {
+            if (count[i] > 0) { return false; }
+        }
+        return true;
+}
