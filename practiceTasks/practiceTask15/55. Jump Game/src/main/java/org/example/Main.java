94e2bec34bd3977e1b8132bd32e2f775bf7fe125
pyrozhenkko <pyrozhenkko@gmail.com>
2025-10-17 23:53:49 +0300

    55. Jump Game(solution)/ 63. Unique Paths II

--- practiceTasks/practiceTask15/55. Jump Game/src/main/java/org/example/Main.java
+++ practiceTasks/practiceTask15/55. Jump Game/src/main/java/org/example/Main.java
@@ -1,11 +1,9 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
         Solution s = new Solution();
-        System.out.println(s.qwe(new int[]{4,3,3,3, 2, 3, 4}));
+        System.out.println(s.qwe(new int[]{1,2,0,3, 2, 3, 4}));
 
     }
 }
@@ -15,15 +13,12 @@ class Solution {
         int lastIndex = nums.length - 1;
         int maxReach = nums[0];
         for (int i = 1; maxReach >= i; i++) {
-            if(maxReach >= lastIndex){
-                return true;
-            }
+            System.out.println("i = " + i + ", maxReach = " + maxReach + ", nums[i] = " + nums[i]);
+            if(maxReach >= lastIndex) return true;
             else{
                 maxReach = Math.max(maxReach, nums[i] + i);
             }
         }
         return false;
     }
-
-
 }
---
+++ practiceTasks/practiceTask17/untitled/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        
+    }
+}
+class Solution{
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+
+    }
+}
