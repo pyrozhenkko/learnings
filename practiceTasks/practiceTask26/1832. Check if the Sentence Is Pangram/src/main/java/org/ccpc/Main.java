b5fa6e5d4abde04b10bc3926edce50c3280f20d6
pyrozhenkko <pyrozhenkko@gmail.com>
2025-11-24 02:30:42 +0200

    1832. Check if the Sentence Is Pangram Solution

--- practiceTasks/practiceTask26/1832. Check if the Sentence Is Pangram/src/main/java/org/ccpc/Main.java
+++ practiceTasks/practiceTask26/1832. Check if the Sentence Is Pangram/src/main/java/org/ccpc/Main.java
@@ -1,6 +1,19 @@
 package org.ccpc;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
 
+    }
+}
+class Solution {
+    public boolean checkIfSentenceIsPangram(String sentence) {
+        Set<Character> set = new HashSet<>();
+        for(int i = 0; i < sentence.length(); i++){
+            set.add(sentence.charAt(i));
+        }
+        return sentence.length() == 26;
     }
 }
