cbb13f103cd7749f07c1247ec5a578f64c6e67a6
pyrozhenkko <pyrozhenkko@gmail.com>
2025-12-08 23:20:57 +0200

    226. Invert Binary Tree

--- practiceTasks/practiceTask38/226. Invert Binary Tree/src/main/java/Main.java
+++ practiceTasks/practiceTask38/226. Invert Binary Tree/src/main/java/Main.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
 }
