c78ea001bc3a24723751e3d8b234f56899175c54
pyrozhenkko <pyrozhenkko@gmail.com>
2025-12-25 07:12:03 +0200

    189. Rotate Array

---
+++ practiceTasks/189. Rotate Array/src/main/java/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
+
+class Solution{
+    public void rotate(int[] nums, int k) {
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+
+    }
+
+    public void reverse(int[] nums, int i, int j){
+        while(i<j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+        i++;
+        j--;
+        }
+    }
+
+}
